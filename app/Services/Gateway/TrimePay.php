20b0ea4d3a719141fef924ff92658a31eefa6e73
Tony Zou <tony200303.zt@gmail.com>
2018-10-13 22:13:03 +0800

    feat: Add support for wechatpay

--- app/Services/Gateway/TrimePay.php
+++ app/Services/Gateway/TrimePay.php
@@ -16,6 +16,7 @@ class TrimePay extends AbstractPayment
 {
 
     private $appSecret;
+    private $gatewayUri;
     /**
      * 签名初始化
      * @param merKey	签名密钥
@@ -23,7 +24,7 @@ class TrimePay extends AbstractPayment
 
     public function __construct($appSecret) {
         $this->appSecret = $appSecret;
-        $this->gatewayUri = 'https://api.Trimepay.com/gateway/pay/go';
+        $this->gatewayUri = 'https://api.Trimepay.com/gateway/';
     }
 
 
@@ -60,7 +61,13 @@ class TrimePay extends AbstractPayment
         }
     }
 
-    public function post($data){
+    public function post($data, $type = "pay"){
+        if ($type == "pay"){
+            $this->gatewayUri .= "pay/go";
+        } else {
+            $this->gatewayUri .= "refund/go";
+        }
+
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $this->gatewayUri);
         curl_setopt($curl, CURLOPT_HEADER, 0);
@@ -92,6 +99,7 @@ class TrimePay extends AbstractPayment
         $pl->tradeno = self::generateGuid();
         $pl->save();
 
+
         $data['appId'] = Config::get('trimepay_appid');
         $data['payType'] = $type;
         $data['merchantTradeNo'] = $pl->tradeno;
@@ -119,11 +127,20 @@ class TrimePay extends AbstractPayment
         $resultVerify = self::verify($str_to_sign, $request->getParam('sign'));
         if ($resultVerify) {
             //file_put_contents('./trimepay_notify_success.log', json_encode($data)."\r\n", FILE_APPEND);
-            self::postPayment($data['merchantTradeNo'], "TrimePay 支付宝");
+            self::postPayment($data['merchantTradeNo'], "TrimePay");
             echo 'SUCCESS';
         }else{
             echo 'FAIL';
         }
+    }
+
+    public function refund($merchantTradeNo){
+        $data['appId'] = Config::get('trimepay_appid');
+        $data['merchantTradeNo'] = $merchantTradeNo;
+        $params = self::prepareSign($data);
+        $data['sign'] = self::sign($params);
+
+        return self::post($data, "refund");
     }
 
 
